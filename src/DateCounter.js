6b125de57e2f8c6b69922fc3178b2d0ed180cd21
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-11-27 15:44:59 +0100

    Trying to wrap my head around useEffect properly

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,42 +2,45 @@ import {useReducer, useState} from 'react';
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  // if (action.type === 'dec') return state - action.payload;
+  // if (action.type === 'inc') return state + action.payload;
+  // if (action.type === 'setCount') return action.payload;
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
   const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date('november 27 2023');
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({type: 'dec'});
     // setCount((count) => count - 1);
     // setCount(count => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({type: 'inc'});
     // setCount((count) => count + 1);
     // setCount(count => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({type: 'setCount', payload: Number(e.target.value)});
     // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
