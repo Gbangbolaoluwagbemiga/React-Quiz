d7499a3c936275a4c9149f9f1567aab2ad415aca
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-11-30 17:38:32 +0100

    Still confused

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,9 +2,16 @@ import {useReducer} from 'react';
 
 function reducer(state, action) {
   console.log(state, action);
-  // if (action.type === 'dec') return state - action.payload;
-  // if (action.type === 'inc') return state + action.payload;
-  // if (action.type === 'setCount') return action.payload;
+  // return {count: 0, step: 1};
+  switch (action.type) {
+    case 'dec':
+      return {...state, count: state.count - 1};
+    case 'inc':
+      return {...state, count: state.count + 1};
+
+    default:
+      console.log('Still confused');
+  }
 }
 
 function DateCounter() {
