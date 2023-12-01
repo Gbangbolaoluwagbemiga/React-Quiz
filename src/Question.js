09fde27c289abf0d2bfb640e9eb5d496225ab20a
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-01 22:12:23 +0100

    question component init

--- src/Question.js
+++ src/Question.js
@@ -1,6 +1,15 @@
 function Question({question}) {
   console.log(question);
-  return <div>Question</div>;
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map(option => (
+          <button className="btn btn-option">{option}</button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Question;
