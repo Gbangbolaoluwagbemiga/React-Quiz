adf8ab8ef04b66665388ea4ef77be919d3268732
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-01 18:12:12 +0100

    Refcatoring of the app component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import {useState} from 'react';
 import DateCounter from './DateCounter';
 
 function App() {
-  const [nums, setNums] = useState(5);
-
-  return (
-    <div className="">
-      <DateCounter />
-    </div>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
