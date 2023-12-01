e9dca33d0190493c2f6b23f9c58344cd6d3db4ef
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-01 21:05:45 +0100

    Question component created

---
+++ src/Question.js
@@ -0,0 +1,5 @@
+function Question() {
+  return <div></div>;
+}
+
+export default Question;
--- src/startScreen.js
+++ src/startScreen.js
@@ -3,7 +3,7 @@ function StartScreeen({numQuest}) {
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuest} questions to test your React mastery</h3>
-      <button>Let's start</button>
+      <button className="btn">Let's start</button>
     </div>
   );
 }
