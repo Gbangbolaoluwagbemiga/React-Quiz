da13b7ea2c6c6a0b6d0decf6cea364d800a2491b
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-01 18:49:50 +0100

    useEffect for loading the Api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {useEffect} from 'react';
 import Header from './Header';
 import Main from './Main';
 
 function App() {
+  useEffect(function () {
+    async function fetcher() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetcher();
+  }, []);
   return (
     <div className="">
       <Header />
