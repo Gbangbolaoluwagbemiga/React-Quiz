98aba08fbb97f3b9506f0973aa3522788664218a
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-01 21:14:12 +0100

    Dispatch passed as props

--- src/startScreen.js
+++ src/startScreen.js
@@ -1,9 +1,11 @@
-function StartScreeen({numQuest}) {
+function StartScreeen({numQuest, dispatch}) {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuest} questions to test your React mastery</h3>
-      <button className="btn">Let's start</button>
+      <button className="btn btn-ui" onClick={() => dispatch({type: 'start'})}>
+        Let's start
+      </button>
     </div>
   );
 }
