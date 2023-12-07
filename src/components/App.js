ddb31af6bd0a45c20df492d062209f144860f548
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-07 20:38:21 +0100

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -96,7 +96,7 @@ function App() {
             <NextButton dispatch={dispatch} answer={answer} />
           </>
         )}
-
+        {index === 15 && dispatch({type: 'completed'})}
         {status === 'completed' && (
           <FinishQuiz totalPoints={totalPoints} points={points} />
         )}
