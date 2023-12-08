e67199c24a41717bd23f794279e6e69499141563
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-08 20:51:02 +0100

    case reset added

--- src/components/App.js
+++ src/components/App.js
@@ -52,6 +52,15 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case 'reset':
+      return {
+        ...state,
+        status: 'loading',
+        index: 0,
+        answer: null,
+        points: 0,
+        highscore: 0,
+      };
 
     default:
       throw new Error('Something went wrong');
