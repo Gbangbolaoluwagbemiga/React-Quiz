638d7df9099ee9e9b8eb64fbe909dd3cd11ee214
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-09 16:15:13 +0100

    Quiz context completed

--- src/components/App.js
+++ src/components/App.js
@@ -12,134 +12,57 @@ import ProgressBar from './ProgressBar';
 import FinishQuiz from './FinishQuiz';
 import Footer from './Footer';
 import Timer from './Timer';
-
-const initialState = {
-  questions: [],
-  status: 'loading',
-  index: 0,
-  answer: null,
-  points: 0,
-  highscore: 0,
-  countDown: null,
-};
-const secPerQuestion = 20;
-const {questions: questApi} = data;
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'dataReceived':
-      return {...state, questions: action.payload, status: 'ready'};
-
-    case 'dataFailed':
-      return {...state, status: 'error'};
-
-    case 'start':
-      return {
-        ...state,
-        status: 'active',
-        countDown: state.questions.length * secPerQuestion,
-      };
-
-    case 'newAnswer':
-      const question = state.questions.at(state.index);
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-    case 'nextQuestion':
-      return {...state, index: state.index + 1, answer: null};
-
-    case 'completed':
-      return {
-        ...state,
-        status: 'finish',
-        highscore:
-          state.points > state.highscore ? state.points : state.highscore,
-      };
-    case 'reset':
-      return {
-        ...initialState,
-        status: 'ready',
-        questions: state.questions,
-      };
-    case 'tick':
-      return {
-        ...state,
-        countDown: state.countDown - 1,
-        status: state.countDown === 0 ? 'finish' : state.status,
-      };
-
-    default:
-      throw new Error('Something went wrong');
-  }
-}
+import {QuizProvider} from '../context/QuizContext';
 
 function App() {
-  const [
-    {questions, status, index, answer, points, highscore, countDown},
-    dispatch,
-  ] = useReducer(reducer, initialState);
-
-  useEffect(function () {
-    setTimeout(() => {
-      dispatch({type: 'dataReceived', payload: questApi});
-    }, 1500);
-  }, []);
-
-  const totalPoints = questions
-    .map(cur => cur.points)
-    .reduce((acc, cur) => acc + cur, 0);
-
   return (
-    <div className="app">
-      <Header />
-      <Main className="main">
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' && (
-          <StartScreen numQuest={questions.length} dispatch={dispatch} />
-        )}
-        {status === 'active' && (
-          <>
-            <ProgressBar
-              numQuest={questions.length}
-              index={index + 1}
-              points={points}
-              questions={questions}
-              answer={answer}
-              totalPoints={totalPoints}
-            />
+    <QuizProvider>
+      <div className="app">
+        <Header />
+        <Main className="main">
+          {status === 'loading' && <Loader />}
+          {status === 'error' && <Error />}
+          {status === 'ready' && (
+            <StartScreen numQuest={questions.length} dispatch={dispatch} />
+          )}
+          {status === 'active' && (
+            <>
+              <ProgressBar
+                numQuest={questions.length}
+                index={index + 1}
+                points={points}
+                questions={questions}
+                answer={answer}
+                totalPoints={totalPoints}
+              />
 
-            <Question
-              dispatch={dispatch}
-              answer={answer}
-              question={questions[index]}
-            />
-            <Footer>
-              <Timer dispatch={dispatch} countDown={countDown} />
-              <NextButton
+              <Question
                 dispatch={dispatch}
                 answer={answer}
-                index={index}
-                numQuest={questions.length}
+                question={questions[index]}
               />
-            </Footer>
-          </>
-        )}
-        {status === 'finish' && (
-          <FinishQuiz
-            totalPoints={totalPoints}
-            points={points}
-            highscore={highscore}
-            dispatch={dispatch}
-          />
-        )}
-      </Main>
-    </div>
+              <Footer>
+                <Timer dispatch={dispatch} countDown={countDown} />
+                <NextButton
+                  dispatch={dispatch}
+                  answer={answer}
+                  index={index}
+                  numQuest={questions.length}
+                />
+              </Footer>
+            </>
+          )}
+          {status === 'finish' && (
+            <FinishQuiz
+              totalPoints={totalPoints}
+              points={points}
+              highscore={highscore}
+              dispatch={dispatch}
+            />
+          )}
+        </Main>
+      </div>
+    </QuizProvider>
   );
 }
 
--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -1,9 +1,104 @@
-import {createContext, useContext} from 'react';
+import {createContext, useContext, useEffect, useReducer} from 'react';
+import data from '../components/questions';
 
 const QuizContext = createContext();
 
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  countDown: null,
+};
+const secPerQuestion = 20;
+const {questions: questApi} = data;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {...state, questions: action.payload, status: 'ready'};
+
+    case 'dataFailed':
+      return {...state, status: 'error'};
+
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+        countDown: state.questions.length * secPerQuestion,
+      };
+
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return {...state, index: state.index + 1, answer: null};
+
+    case 'completed':
+      return {
+        ...state,
+        status: 'finish',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case 'reset':
+      return {
+        ...initialState,
+        status: 'ready',
+        questions: state.questions,
+      };
+    case 'tick':
+      return {
+        ...state,
+        countDown: state.countDown - 1,
+        status: state.countDown === 0 ? 'finish' : state.status,
+      };
+
+    default:
+      throw new Error('Something went wrong');
+  }
+}
+
 function QuizProvider({children}) {
-  return <QuizContext.Provider value={{}}>{children}</QuizContext.Provider>;
+  const [
+    {questions, status, index, answer, points, highscore, countDown},
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    setTimeout(() => {
+      dispatch({type: 'dataReceived', payload: questApi});
+    }, 1500);
+  }, []);
+
+  const totalPoints = questions
+    .map(cur => cur.points)
+    .reduce((acc, cur) => acc + cur, 0);
+  return (
+    <QuizContext.Provider
+      value={{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highscore,
+        countDown,
+        totalPoints,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
 }
 
 function UseQuiz() {
